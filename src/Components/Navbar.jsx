4fc0e602daa6237ae469ddcb5e51e80fcb195411
Krushna <paratekrushna26@gmail.com>
2025-02-28 13:14:52 +0530

    Updated project with new changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo */}
         <a href="#" className="text-2xl font-semibold text-gray-800">
-          ShopLogo
+          TechStore
         </a>
 
         {/* Navigation Links */}
